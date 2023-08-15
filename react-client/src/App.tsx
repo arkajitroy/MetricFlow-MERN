5107ef38032bfbc554f61c75d3200583def9cdc0
arkajitroy <arkajitroy11.official@gmail.com>
2023-08-15 09:54:23 +0530

    configured: theme reducer with app

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { themeSettings } from "./global/theme";
 
 const App = () => {
-  return <div>App</div>;
+  const mode = useSelector((state: any) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default App;
